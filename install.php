7f57599dbbf13847cd57faf827d82b4f3af26711
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-16 09:05:02 -0500

    Update for license fileUpdate for license file

--- install.php
+++ install.php
@@ -1,7 +1,5 @@
 <?php
-//	License for all code of this FreePBX module can be found in the license file inside the module directory
-//	Copyright 2013 Schmooze Com Inc.
-//
+
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 
 global $db;
--- page.callrecording.php
+++ page.callrecording.php
@@ -1,9 +1,6 @@
 <?php 
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
-//	License for all code of this FreePBX module can be found in the license file inside the module directory
-//  Copyright 2006 Philippe Lindheimer - Astrogen LLC
-//	Copyright 2013 Schmooze Com Inc.
-//
+
 $type = isset($_REQUEST['type']) ? $_REQUEST['type'] : 'setup';
 $action = isset($_REQUEST['action']) ? $_REQUEST['action'] :  '';
 if (isset($_REQUEST['delete'])) $action = 'delete'; 
