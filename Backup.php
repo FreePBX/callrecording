a5cf024432448c0b4a761066fce2e9e386784915
James Finstrom <jfinstrom@sangoma.com>
2018-07-10 21:43:28 +0000

    Fix backup variables

--- Backup.php
+++ Backup.php
@@ -4,8 +4,8 @@ use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
   public function runBackup($id,$transaction){
     $configs = [];
-    $configs['rules'] = $this->FrePBX->Callrecording->listAll();
-    $configs['modules'] = $this->FrePBX->Callrecording->dumpExtensions();
+    $configs['rules'] = $this->FreePBX->Callrecording->listAll();
+    $configs['modules'] = $this->FreePBX->Callrecording->dumpExtensions();
     $this->addConfigs($configs);
   }
 }
