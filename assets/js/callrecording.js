4a9aaed10d84d65f0a2e6a466094459fa33019e9
Franck Danard <fdanard@sangoma.com>
2019-11-22 13:45:54 +0000

    FREEI-913 Fix XSS Bootstrap Datatable Issues
    Conflicts:
    	assets/js/callrecording.js

--- assets/js/callrecording.js
+++ assets/js/callrecording.js
@@ -22,3 +22,14 @@ function checkCallRecording(theForm) {
 	}
 	return true;
 }
+
+function linkFormatter(value, row, index) {
+	return decodeHTML(value);
+}
+
+function decodeHTML(data) {
+	var textArea = document.createElement('textarea');
+	textArea.innerHTML = data;
+	return textArea.value;
+}
+
